d21b49e14c6d6b2109b8fdaeed5924b74ad6c7b7
srnfr <stephane@reytan.fr>
2021-08-03 15:57:12 +0200

    Ver 0.98f

--- fetch-GCP2PT.sh
+++ fetch-GCP2PT.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ver="0.98e"
+ver="0.98f"
 # author=srn
 
 #-------
@@ -67,6 +67,8 @@ mkdir -p "$iddir"
 # fetching logs from GCP and testing JSON validity
 gcloud logging read "resource.type=\"audited_resource\"" --organization="$organization" --freshness="$fresh" --format json > "$iddir"/gcp.log
 
+[ $debug == 1 ] && ls -lh "$iddir"/gcp.log
+
 if jq empty "$iddir"/gcp.log; then
   [ $debug == 1 ] &&  echo "JSON from GCP is valid"
 else
@@ -74,10 +76,10 @@ else
   exit
 fi
 
+[ $debug == 1 ] && ls -lh "$iddir"/logGCP.json.lst
 [ $debug == 1 ] &&  echo "Now parsing the logs bulk ("$iddir"/gcp.log)... "
 if [ -s "$iddir"/gcp.log ]; then
-	cat "$iddir"/gcp.log | jq -Mc > "$iddir"/logGCP.json.lst
-        [ $debug == 1 ] && ls -lh "$iddir"/logGCP.json.lst
+	cat "$iddir"/gcp.log | jq -Mcl "." > "$iddir"/logGCP.json.lst
 else
 	[ $debug == 1 ] && echo "Bulk log file exmpty. Now exiting"
 	exit
